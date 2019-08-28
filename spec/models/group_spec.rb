81f73cd9120b207a22236d4cfc5d2e9a01aa9566
kizuku <tukada2947@yahoo.co.jp>
2019-08-28 17:21:12 +0900

    factory_bot導入

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,5 +6,16 @@ describe Group do
       group.valid?
       expect(group.errors[:name]).to include("を入力してください")
     end
+
+    it "nameが15文字なら登録できる" do
+      group = Group.new(:group, name: "aaaaaaaaaaaaaaa" )
+      expect(group).to be_valid
+    end
+
+    it "nameが15文字を超える場合は登録できない" do
+      group = Group.new(:group, name: "aaaaaaaaaaaaaaaa" )
+      group.valid?
+      expect(group.errors[:name]).to include("は15文字以内で入力してください")
+    end
   end
 end
