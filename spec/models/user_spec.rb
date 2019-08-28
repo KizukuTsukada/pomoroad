fa2113add73ed2132f9dec3976ebf6b88830d5fd
kizuku <tukada2947@yahoo.co.jp>
2019-08-28 15:40:42 +0900

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "nameが空欄なら無効" do
-    end
+  it "nameが空の場合、登録できない" do
+    user = build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("を入力してください")
+  end
   end
 end
