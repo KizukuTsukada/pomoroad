a53aca71ba7e26fdc0891548579a52b85d2ff25d
kizuku <tukada2947@yahoo.co.jp>
2019-08-28 18:22:03 +0900

    モデルテスト

---
+++ spec/models/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    name                  {"taro"}
+    email                 {"aaa@gmail.com"}
+    password              {"000000"}
+    password_confirmation {"000000"}
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,40 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "nameが空の時は登録できない" do
-      user = User.new(name: "")
-      user.valid?
-      expect(user.errors[:name]).to include("を入力してください")
-    end
+  it "nameが空の時は登録できない" do
+    user = build(:user, name: "")
+    user.valid?
+    expect(user.errors[:name]).to include("を入力してください")
+  end
+  it "emailが空の時は登録できない" do
+    user = build(:email, name: "")
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
 
-    it "emailが空の時は登録できない" do
-      user = User.new(email: "")
-      user.valid?
-      expect(user.errors[:email]).to include("を入力してください")
-    end
+  it "passwordが空の時は登録できない" do
+    user = build(:password, name: "")
+    user.valid?
+    expect(user.errors[:password]).to include("を入力してください")
+  end
 
-    it "passwordが空の時は登録できない" do
-      user = User.new(password: "")
-      user.valid?
-      expect(user.errors[:password]).to include("を入力してください")
-    end
+  # it "nameが空の時は登録できない" do
+  #   user = User.new(name: "")
+  #   user.valid?
+  #   expect(user.errors[:name]).to include("を入力してください")
+  # end
+
+  # it "emailが空の時は登録できない" do
+  #   user = User.new(email: "")
+  #   user.valid?
+  #   expect(user.errors[:email]).to include("を入力してください")
+  # end
+
+  # it "passwordが空の時は登録できない" do
+  #   user = User.new(password: "")
+  #   user.valid?
+  #   expect(user.errors[:password]).to include("を入力してください")
+  # end
+
   end
 end
