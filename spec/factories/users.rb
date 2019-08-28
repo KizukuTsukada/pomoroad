6862a381e5e3a94ce2211632658ff7dec0d05b38
kizuku <tukada2947@yahoo.co.jp>
2019-08-28 20:46:06 +0900

    テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
-
   factory :user do
     name                  {"taro"}
     email                 {"aaa@gmail.com"}
     password              {"000000"}
     password_confirmation {"000000"}
   end
-
 end
