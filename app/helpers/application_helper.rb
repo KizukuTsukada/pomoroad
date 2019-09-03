b2e25c399e06a0c4d2a2c32ba432e111c791023e
kizuku <tukada2947@yahoo.co.jp>
2019-09-03 13:48:17 +0900

    修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,49 +1,22 @@
 module ApplicationHelper
-  require "redcarpet"
-  require "coderay"
-
-  class HTMLwithCoderay < Redcarpet::Render::HTML
-      def block_code(code, language)
-
-        if language.include?(':')
-          
-          language = language.split(':')[0]
-            case language.to_s
-            when 'rb'
-                lang = 'ruby'
-            when 'yml'
-                lang = 'yaml'
-            when 'css'
-                lang = 'css'
-            when 'html'
-                lang = 'html'
-            when ''
-                lang = 'md'
-            else
-                lang = language
-            end
-
-          CodeRay.scan(code, lang).div
-        else
-          return code
-        end
-      end
-  end
-
   def markdown(text)
-      html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
       options = {
-          autolink: true,
-          space_after_headers: true,
-          no_intra_emphasis: true,
-          fenced_code_blocks: true,
-          tables: true,
-          hard_wrap: true,
-          xhtml: true,
-          lax_html_blocks: true,
-          strikethrough: true
+          filter_html:     true,
+          hard_wrap:       true,
+          space_after_headers: true
       }
-      markdown = Redcarpet::Markdown.new(html_render, options)
-      markdown.render(text)
+
+      extensions = {
+          autolink: true,
+          fenced_code_blocks: true,
+          lax_spacing: true,
+          no_intra_emphasis: true,
+          strikethrough: true,
+          superscript: true,
+          tables: true
+      }
+      renderer = Redcarpet::Render::HTML.new(options)
+      @markdown = Redcarpet::Markdown.new(renderer, extensions)
+    @markdown.render(text).html_safe
   end
 end
